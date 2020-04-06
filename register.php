b4f867715e897d888304d6ee50d22012c4393b05
devendrachandora <63260222+devendrachandora@users.noreply.github.com>
2020-04-07 02:11:31 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://Devendracbot.herokuapp.com/execute.php';
+$WEBHOOK_URL = 'https://meraapp1.herokuapp.com/execute.php';
 $BOT_TOKEN = '{1173706479:AAERBbIN-1Q-0RVfrp1cKaloefGbOoqrx00}';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
